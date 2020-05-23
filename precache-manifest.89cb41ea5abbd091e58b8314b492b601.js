fdbeb48db5b8cf114e76d41b2311536b05084054
unknown <dnicerio@gmail.com>
2020-05-23 20:41:34 +0800

    Updates

---
+++ precache-manifest.89cb41ea5abbd091e58b8314b492b601.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb6096461820a8f9937f82bfe606405",
+    "url": "/todolist-redo/index.html"
+  },
+  {
+    "revision": "c87f48ad39b90dcbc4d1",
+    "url": "/todolist-redo/static/css/2.ff668ddd.chunk.css"
+  },
+  {
+    "revision": "32bbe109d2eb9c428814",
+    "url": "/todolist-redo/static/css/main.9cceebfa.chunk.css"
+  },
+  {
+    "revision": "c87f48ad39b90dcbc4d1",
+    "url": "/todolist-redo/static/js/2.c6c28f8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-redo/static/js/2.c6c28f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32bbe109d2eb9c428814",
+    "url": "/todolist-redo/static/js/main.114c7a01.chunk.js"
+  },
+  {
+    "revision": "ba5cf88caf539855d5fe",
+    "url": "/todolist-redo/static/js/runtime-main.8612edda.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todolist-redo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todolist-redo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todolist-redo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todolist-redo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todolist-redo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
